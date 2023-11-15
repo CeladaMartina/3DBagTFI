c6ad708c6245ea3bd7963a9c27aeefa9eb3af45d
Martina celada <mcelada@quadiontech.com>
2023-11-15 12:50:30 -0300

    prueba ante final}

--- 3DBag/PatternChain/Administrador.cs
+++ 3DBag/PatternChain/Administrador.cs
@@ -15,7 +15,7 @@ namespace _3DBag.PatternChain
                 && Propiedades_BE.SingletonLogin.GetInstance.IsInRole(Propiedades_BE.TipoPermiso.Modificar_Patentes)
                 && Propiedades_BE.SingletonLogin.GetInstance.IsInRole(Propiedades_BE.TipoPermiso.Modificar_Articulo)
                 && Propiedades_BE.SingletonLogin.GetInstance.IsInRole(Propiedades_BE.TipoPermiso.Modificar_Venta))
-            {
+            {                
                 return "Administrador";
             }
             else
